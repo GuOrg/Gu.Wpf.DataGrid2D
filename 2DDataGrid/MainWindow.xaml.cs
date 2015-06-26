03047df964c2d8fe50a86cc36a80a25d46f64399
Johan Larsson <johan_gavle1@hotmail.com>
2015-06-26 18:13:10 +0200

    Initial

---
+++ 2DDataGrid/MainWindow.xaml.cs
@@ -0,0 +1,15 @@
+﻿namespace _2DDataGrid
+{
+    using System.Windows;
+
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ 2DDataGrid/Vm.cs
@@ -0,0 +1,20 @@
+﻿namespace _2DDataGrid
+{
+    public class Vm
+    {
+        public Vm()
+        {
+            Data = new[,] { { 1, 2 }, { 3, 4 } };
+            ColumnHeaders = new[] { "Col1", "Col2" };
+            Rows = new int[2][];
+            Rows[0] = new[] { 1, 2 };
+            Rows[1] = new[] { 3, 4 };
+        }
+
+        public string[] ColumnHeaders { get; private set; }
+        
+        public int[][] Rows { get; private set; }
+
+        public int[,] Data { get; private set; }
+    }
+}
