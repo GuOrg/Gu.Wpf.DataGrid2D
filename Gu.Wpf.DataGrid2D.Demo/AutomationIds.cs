86681b9445c1b629da1754a3bde47a9efad884ee
Johan Larsson <johan_gavle1@hotmail.com>
2015-12-22 14:19:15 +0100

    WIP

--- Gu.Wpf.DataGrid2D.Demo/AutomationIds.cs
+++ Gu.Wpf.DataGrid2D.Demo/AutomationIds.cs
@@ -5,9 +5,10 @@
     public static class AutomationIds
     {
         public static readonly string MainWindow = Create();
-        public static readonly string VanillaGroupBox = Create();
         public static readonly string MultiDimensionalTab = Create();
         public static readonly string MultiDimensionalAutoColumns = Create();
+        public static readonly string MultiDimensionalExplicitColumns = Create();
+        public static readonly string MultiDimensionalWithHeaders = Create();
 
         private static string Create([CallerMemberName] string name = null)
         {
