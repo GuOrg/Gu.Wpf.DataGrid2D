32ddeda6d69e5829f0fe705ddfdd4b4f6fbb88e5
Johan Larsson <johan_gavle1@hotmail.com>
2015-12-24 14:18:57 +0100

    Add transposed row test.

--- Gu.Wpf.DataGrid2D.UiTests/ItemsSourceTests.Transposed.cs
+++ Gu.Wpf.DataGrid2D.UiTests/ItemsSourceTests.Transposed.cs
@@ -136,7 +136,7 @@
             }
 
             [Test]
-            public void ObservableCollectionRemoveRowInDataContext()
+            public void RemoveRowInDataContext()
             {
                 using (var app = Application.AttachOrLaunch(Info.ProcessStartInfo))
                 {
@@ -163,6 +163,44 @@
 
                     Assert.AreEqual("Johan", dataGrid.Cell(c1, 0).Text);
                     Assert.AreEqual("Larsson", dataGrid.Cell(c1, 1).Text);
+                }
+            }
+
+            [Test]
+            public void AddRowInDataContext()
+            {
+                using (var app = Application.AttachOrLaunch(Info.ProcessStartInfo))
+                {
+                    var window = app.GetWindow(AutomationIds.MainWindow, InitializeOption.NoCache);
+                    var page = window.Get<TabPage>(AutomationIds.TransposedTab);
+                    page.Select();
+
+                    var reference = page.Get<ListView>(AutomationIds.ReferenceDataGrid);
+                    var cell = reference.Rows[2].Cells[0];
+                    cell.Click();
+                    cell.Enter("New");
+                    reference.Select(0);
+                    var dataGrid = page.Get<ListView>(AutomationIds.TransposedObservableCollection);
+
+                    Assert.AreEqual(4, dataGrid.Rows[0].Cells.Count);
+                    Assert.AreEqual(2, dataGrid.Rows.Count);
+
+                    var c0 = dataGrid.Header.Columns[0].Text;
+                    var c1 = dataGrid.Header.Columns[1].Text;
+                    var c2 = dataGrid.Header.Columns[2].Text;
+                    var c3 = dataGrid.Header.Columns[3].Text;
+
+                    Assert.AreEqual("FirstName", dataGrid.Cell(c0, 0).Text);
+                    Assert.AreEqual("LastName", dataGrid.Cell(c0, 1).Text);
+
+                    Assert.AreEqual("Johan", dataGrid.Cell(c1, 0).Text);
+                    Assert.AreEqual("Larsson", dataGrid.Cell(c1, 1).Text);
+
+                    Assert.AreEqual("Erik", dataGrid.Cell(c2, 0).Text);
+                    Assert.AreEqual("Svensson", dataGrid.Cell(c2, 1).Text);
+
+                    Assert.AreEqual("New", dataGrid.Cell(c3, 0).Text);
+                    Assert.AreEqual("", dataGrid.Cell(c3, 1).Text);
                 }
             }
 
