32465ccc5aa64fe5872f1c598bca667a6527ca21
Johan Larsson <johan_gavle1@hotmail.com>
2016-07-17 21:54:07 +0200

    paket auto-restore on

---
+++ Gu.Wpf.DataGrid2D.Demo/BigDataGridView.xaml.cs
@@ -0,0 +1,17 @@
+﻿namespace Gu.Wpf.DataGrid2D.Demo
+{
+    using System.Windows.Controls;
+
+    public partial class BigDataGridView : UserControl
+    {
+        public BigDataGridView()
+        {
+            this.InitializeComponent();
+        }
+    }
+
+    public class BigDataGridViewModel
+    {
+        public static readonly BigDataGridViewModel Default = new BigDataGridViewModel();
+    }
+}
