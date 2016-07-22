8e673fc969afb2aec632628918a10be667fe7831
menno.van.der.woude@rhdhv.com <menno.van.der.woude@rhdhv.com>
2016-07-22 17:44:17 +0200

    Added two attached depency properties: CellTemplate and CellEditingTemlate. These can be used to specify a custom template for the cells of the colums of the 2D grid. It causes the grid to use TemplateColumns instead of TextColumns while autogenerating columns.

---
+++ Gu.Wpf.DataGrid2D/ItemsSource.CellEditingTemplate.cs
@@ -0,0 +1,39 @@
+﻿namespace Gu.Wpf.DataGrid2D
+{
+    using System;
+    using System.Windows;
+    using System.Windows.Controls;
+    using System.Windows.Data;
+
+    public static partial class ItemsSource
+    {
+        public static readonly DependencyProperty CellEditingTemplateProperty = DependencyProperty.RegisterAttached(
+            "CellEditingTemplate",
+            typeof(DataTemplate),
+            typeof(ItemsSource),
+            new PropertyMetadata(null, OnCellEditingTemplateChanged),
+            OnValidateCellEditingTemplate);
+
+        public static void SetCellEditingTemplate(this DataGrid element, DataTemplate value)
+        {
+            element.SetValue(CellEditingTemplateProperty, value);
+        }
+
+        [AttachedPropertyBrowsableForChildren(IncludeDescendants = false)]
+        [AttachedPropertyBrowsableForType(typeof(DataGrid))]
+        public static DataTemplate GetCellEditingTemplate(this DataGrid element)
+        {
+            return (DataTemplate)element.GetValue(CellEditingTemplateProperty);
+        }
+
+        private static void OnCellEditingTemplateChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            // TODO
+        }
+
+        private static bool OnValidateCellEditingTemplate(object value)
+        {
+            return true;
+        }
+    }
+}
