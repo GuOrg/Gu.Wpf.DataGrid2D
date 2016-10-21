100e889c7b323cb9f8c53dd592979851a60aeed6
Johan Larsson <johan_gavle1@hotmail.com>
2016-10-21 23:15:04 +0200

    CellTemplateDemoClass : inpc

--- Gu.Wpf.DataGrid2D.Demo/CellTemplateDemoClass.cs
+++ Gu.Wpf.DataGrid2D.Demo/CellTemplateDemoClass.cs
@@ -1,13 +1,79 @@
 ﻿namespace Gu.Wpf.DataGrid2D.Demo
 {
+    using System.ComponentModel;
+    using System.Runtime.CompilerServices;
     using System.Windows.Media;
+    using JetBrains.Annotations;
 
-    public class CellTemplateDemoClass
+    public class CellTemplateDemoClass : INotifyPropertyChanged
     {
-        public int Value1 { get; set; }
+        private int value1;
+        private int value2;
+        private SolidColorBrush background;
 
-        public int Value2 { get; set; }
+        public event PropertyChangedEventHandler PropertyChanged;
 
-        public SolidColorBrush Background { get; set; }
+        public int Value1
+        {
+            get
+            {
+                return this.value1;
+            }
+
+            set
+            {
+                if (value == this.value1)
+                {
+                    return;
+                }
+
+                this.value1 = value;
+                this.OnPropertyChanged();
+            }
+        }
+
+        public int Value2
+        {
+            get
+            {
+                return this.value2;
+            }
+
+            set
+            {
+                if (value == this.value2)
+                {
+                    return;
+                }
+
+                this.value2 = value;
+                this.OnPropertyChanged();
+            }
+        }
+
+        public SolidColorBrush Background
+        {
+            get
+            {
+                return this.background;
+            }
+
+            set
+            {
+                if (Equals(value, this.background))
+                {
+                    return;
+                }
+
+                this.background = value;
+                this.OnPropertyChanged();
+            }
+        }
+
+        [NotifyPropertyChangedInvocator]
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            this.PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
     }
 }
