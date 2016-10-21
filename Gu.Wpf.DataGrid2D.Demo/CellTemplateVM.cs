659501a6a1ccbf6f15533ca9a42a1be6f31880c7
menno.van.der.woude@rhdhv.com <menno.van.der.woude@rhdhv.com>
2016-10-21 22:43:25 +0200

    Small improvements to demo of cell template property

--- Gu.Wpf.DataGrid2D.Demo/CellTemplateVM.cs
+++ Gu.Wpf.DataGrid2D.Demo/CellTemplateVM.cs
@@ -1,39 +1,28 @@
 ﻿namespace Gu.Wpf.DataGrid2D.Demo
 {
-    using JetBrains.Annotations;
     using System;
     using System.ComponentModel;
     using System.Runtime.CompilerServices;
-    using System.Windows;
     using System.Windows.Input;
     using System.Windows.Media;
-
-    public class DemoClass
-    {
-        public int Value1 { get; set; }
-
-        public int Value2 { get; set; }
-
-        public SolidColorBrush Background { get; set; }
-    }
+    using JetBrains.Annotations;
 
     public class CellTemplateVm : INotifyPropertyChanged
     {
         private string data;
-        private int currenttemplate = 3;
 
         public CellTemplateVm()
         {
             this.RowHeaders = new[] { "1", "2", "3" };
             this.ColumnHeaders = new[] { "A", "B", "C" };
 
-            this.Data2D = new DemoClass[3, 3];
+            this.Data2D = new CellTemplateDemoClass[3, 3];
             Random r = new Random();
             for (int i = 0; i < 3; ++i)
             {
                 for (int j = 0; j < 3; ++j)
                 {
-                    DemoClass cl = new DemoClass();
+                    CellTemplateDemoClass cl = new CellTemplateDemoClass();
                     cl.Value1 = i + j;
                     cl.Value2 = 9 - j - i;
 
@@ -61,9 +50,7 @@
 
         public ICommand UpdateDataCommand { get; }
 
-        public ICommand ChangeCellTemplateCommand { get; }
-
-        public DemoClass[,] Data2D { get; }
+        public CellTemplateDemoClass[,] Data2D { get; }
 
         public string Data
         {
