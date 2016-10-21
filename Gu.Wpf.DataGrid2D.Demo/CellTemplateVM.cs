95f791ba31b0efcfe296963a5b60a4a3afae966c
menno.van.der.woude@rhdhv.com <menno.van.der.woude@rhdhv.com>
2016-10-21 22:30:55 +0200

    Added more examples for using CellTemplate and CellEditingTemplate attached properties

--- Gu.Wpf.DataGrid2D.Demo/CellTemplateVM.cs
+++ Gu.Wpf.DataGrid2D.Demo/CellTemplateVM.cs
@@ -1,20 +1,26 @@
 ﻿namespace Gu.Wpf.DataGrid2D.Demo
 {
     using JetBrains.Annotations;
+    using System;
     using System.ComponentModel;
     using System.Runtime.CompilerServices;
+    using System.Windows;
     using System.Windows.Input;
+    using System.Windows.Media;
 
     public class DemoClass
     {
-        public string Value1 { get; set; }
+        public int Value1 { get; set; }
 
-        public string Value2 { get; set; }
+        public int Value2 { get; set; }
+
+        public SolidColorBrush Background { get; set; }
     }
 
     public class CellTemplateVm : INotifyPropertyChanged
     {
         private string data;
+        private int currenttemplate = 3;
 
         public CellTemplateVm()
         {
@@ -22,18 +28,28 @@
             this.ColumnHeaders = new[] { "A", "B", "C" };
 
             this.Data2D = new DemoClass[3, 3];
+            Random r = new Random();
             for (int i = 0; i < 3; ++i)
             {
                 for (int j = 0; j < 3; ++j)
                 {
                     DemoClass cl = new DemoClass();
-                    cl.Value1 = i.ToString() + j.ToString();
-                    cl.Value2 = j.ToString() + i.ToString();
+                    cl.Value1 = i + j;
+                    cl.Value2 = 9 - j - i;
+
+                    cl.Background = new SolidColorBrush(new Color()
+                    {
+                        A = (byte)r.Next(255),
+                        R = (byte)r.Next(255),
+                        G = (byte)r.Next(255),
+                        B = (byte)r.Next(255)
+                    });
                     this.Data2D[i, j] = cl;
                 }
             }
 
             this.UpdateDataCommand = new RelayCommand(this.UpdateData);
+
             this.UpdateData();
         }
 
@@ -44,6 +60,8 @@
         public string[] ColumnHeaders { get; }
 
         public ICommand UpdateDataCommand { get; }
+
+        public ICommand ChangeCellTemplateCommand { get; }
 
         public DemoClass[,] Data2D { get; }
 
@@ -74,7 +92,7 @@
 
         private void UpdateData()
         {
-
+            // ? todo
         }
     }
 }
