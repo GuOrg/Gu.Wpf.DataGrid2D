62c9afa20808a2ed3d126fb58056d669a9ad09ce
menno.van.der.woude@rhdhv.com <menno.van.der.woude@rhdhv.com>
2016-10-21 22:43:25 +0200

    Small improvements to demo of cell template property

---
+++ Gu.Wpf.DataGrid2D.Demo/CellTemplateDemoClass.cs
@@ -0,0 +1,13 @@
+﻿namespace Gu.Wpf.DataGrid2D.Demo
+{
+    using System.Windows.Media;
+
+    public class CellTemplateDemoClass
+    {
+        public int Value1 { get; set; }
+
+        public int Value2 { get; set; }
+
+        public SolidColorBrush Background { get; set; }
+    }
+}
