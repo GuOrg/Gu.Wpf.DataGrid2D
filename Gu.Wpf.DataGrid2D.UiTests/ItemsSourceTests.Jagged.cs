9ebd3b211d49ef5737fc6a5965eb8a314a3646db
Johan Larsson <johan_gavle1@hotmail.com>
2018-04-13 06:43:12 +0200

    UI-test setting column headers to null.

--- Gu.Wpf.DataGrid2D.UiTests/ItemsSourceTests.Jagged.cs
+++ Gu.Wpf.DataGrid2D.UiTests/ItemsSourceTests.Jagged.cs
@@ -202,6 +202,74 @@ namespace Gu.Wpf.DataGrid2D.UiTests
                     Assert.AreEqual("6", dataGrid[2, 1].Value);
                 }
             }
+
+            [Test]
+            public void ToggleColumnHeaders()
+            {
+                using (var app = Application.Launch(Info.ExeFileName, "JaggedWindow"))
+                {
+                    var window = app.MainWindow;
+                    var dataGrid = window.FindDataGrid("WithHeaders");
+                    var useHeadersButton = window.FindButton("Use column headers");
+                    var nullHeadersButton = window.FindButton("Null column headers");
+
+                    var columnHeaders = dataGrid.ColumnHeaders;
+                    Assert.AreEqual(2, columnHeaders.Count);
+                    Assert.AreEqual("A", columnHeaders[0].Text);
+                    Assert.AreEqual("B", columnHeaders[1].Text);
+
+                    var rows = dataGrid.Rows;
+                    Assert.AreEqual(3, rows.Count);
+                    Assert.AreEqual("1", rows[0].Header.Text);
+                    Assert.AreEqual("2", rows[1].Header.Text);
+                    Assert.AreEqual("3", rows[2].Header.Text);
+
+                    Assert.AreEqual("1", dataGrid[0, 0].Value);
+                    Assert.AreEqual("2", dataGrid[0, 1].Value);
+                    Assert.AreEqual("3", dataGrid[1, 0].Value);
+                    Assert.AreEqual("4", dataGrid[1, 1].Value);
+                    Assert.AreEqual("5", dataGrid[2, 0].Value);
+                    Assert.AreEqual("6", dataGrid[2, 1].Value);
+
+                    nullHeadersButton.Invoke();
+                    columnHeaders = dataGrid.ColumnHeaders;
+                    Assert.AreEqual(2, columnHeaders.Count);
+                    Assert.AreEqual(string.Empty, columnHeaders[0].Text);
+                    Assert.AreEqual(string.Empty, columnHeaders[1].Text);
+
+                    rows = dataGrid.Rows;
+                    Assert.AreEqual(3, rows.Count);
+                    Assert.AreEqual("1", rows[0].Header.Text);
+                    Assert.AreEqual("2", rows[1].Header.Text);
+                    Assert.AreEqual("3", rows[2].Header.Text);
+
+                    Assert.AreEqual("1", dataGrid[0, 0].Value);
+                    Assert.AreEqual("2", dataGrid[0, 1].Value);
+                    Assert.AreEqual("3", dataGrid[1, 0].Value);
+                    Assert.AreEqual("4", dataGrid[1, 1].Value);
+                    Assert.AreEqual("5", dataGrid[2, 0].Value);
+                    Assert.AreEqual("6", dataGrid[2, 1].Value);
+
+                    useHeadersButton.Invoke();
+                    columnHeaders = dataGrid.ColumnHeaders;
+                    Assert.AreEqual(2, columnHeaders.Count);
+                    Assert.AreEqual("A", columnHeaders[0].Text);
+                    Assert.AreEqual("B", columnHeaders[1].Text);
+
+                    rows = dataGrid.Rows;
+                    Assert.AreEqual(3, rows.Count);
+                    Assert.AreEqual("1", rows[0].Header.Text);
+                    Assert.AreEqual("2", rows[1].Header.Text);
+                    Assert.AreEqual("3", rows[2].Header.Text);
+
+                    Assert.AreEqual("1", dataGrid[0, 0].Value);
+                    Assert.AreEqual("2", dataGrid[0, 1].Value);
+                    Assert.AreEqual("3", dataGrid[1, 0].Value);
+                    Assert.AreEqual("4", dataGrid[1, 1].Value);
+                    Assert.AreEqual("5", dataGrid[2, 0].Value);
+                    Assert.AreEqual("6", dataGrid[2, 1].Value);
+                }
+            }
         }
     }
 }
